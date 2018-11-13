0b6d3f5394ec6cade9d5a17abd7dcdd70ec3d98d
sallysendiri <44987899+sallysendiri@users.noreply.github.com>
2018-11-13 09:34:18 +0700

    Update fff.php

--- fff.php
+++ fff.php
@@ -37,8 +37,8 @@ if($ext->status <> 'ok') {
     $req         = proccess(1, $useragent, 'feed/timeline/', $cookie);
     $is_verified = (json_decode($req[1])->status <> ok) ? 0 : 1;
     $uid         = $ext->logged_in_user->pk;
-    echo ".$yellow."[+] Masokk Pak Ekooo....".$normal."\n";
-    echo ".$green."[+] Sek Lurr".$normal."\n";
+    echo "".$white."[+] Masokk Pak Ekooo....".$normal."\n";
+    echo "".$green."[+] Sek Lurr".$normal."\n";
     sleep(1);
     $data  = array(
         'aksi' => 'addig',
